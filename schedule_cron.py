e38213601ea09b1bd14dac02f78ab0e93367cbcb
Shaun Cox <seyyes2000@hotmail.com>
2017-11-27 15:40:11 -0500

    finish cron schedule

--- schedule_cron.py
+++ schedule_cron.py
@@ -2,6 +2,7 @@ from crontab import CronTab
 
 
 my_cron = CronTab(user='shauncox')
+job = my_cron.new(command='python3 /Users/shauncox/cron_job/write_date.py')
+job.minute.every(1)
 
-for job in my_cron:
-	print (job)
+my_cron.write()
