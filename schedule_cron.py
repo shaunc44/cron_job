fb818c1df224fac1b5ca027f12e064b209481690
Shaun Cox <seyyes2000@hotmail.com>
2017-11-27 16:18:08 -0500

    add remove cron job method

--- schedule_cron.py
+++ schedule_cron.py
@@ -2,7 +2,13 @@ from crontab import CronTab
 
 
 my_cron = CronTab(user='shauncox')
-job = my_cron.new(command='python3 /Users/shauncox/cron_job/write_date.py')
+
+job = my_cron.new(
+		command = 'python3 /Users/shauncox/cron_job/write_date.py', 
+		comment = 'date_info'
+	)
 job.minute.every(1)
+
+# my_cron.remove_all()
 
 my_cron.write()
